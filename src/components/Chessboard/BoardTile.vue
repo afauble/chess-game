5e6eb6e5208aa632371f217a4037bd6eb99bfe33
afauble <andrew.fauble@gmail.com>
2023-11-20 18:20:49 -0500

    Added the ability to move pieces with clicks.
    Added piece icons
    Changed the json input to add the color of the piece
    Created the ability to move pieces by updating the img on clicked tiles.

--- src/components/Chessboard/BoardTile.vue
+++ src/components/Chessboard/BoardTile.vue
@@ -1,35 +1,42 @@
 <template>
-    <div :class="tileColorStr">
-        <div class="center">
-            {{ props.pieceName }}
+    <div :class="tileColorStr" @click="selectPiece">
+        <div class="sizeContent">
+            <img class="sizeContent" v-if="imageStr" :src="imageStr" alt="" draggable="false"/>
         </div>
     </div>
 </template>
 
 <style scoped>
-
-    .center {
-        position: relative;
-        float: left;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
+    .sizeContent {
+        height: 100%;
+        width: 100%;
     }
-
+    .sizeContent img {
+        height: 100%;
+        width: 100%;
+    }
     .whiteTile {
-        background-color: white;
+        background-color: rgb(170, 220, 180);
         color: black;
     }
-
     .blackTile {
-        background-color: black;
+        background-color: rgb(50, 130, 85);
         color: white;
+    }
+    .selectedTile {
+        background-color: rgb(230, 220, 60);
     }
 
 </style>
 
 <script setup>
+import { computed } from 'vue';
+
     const props = defineProps({
+        index: {
+            type: Number,
+            required: true
+        },
         cordinates: {
             type: Array,
             required: true
@@ -38,13 +45,76 @@
             type: Boolean,
             required: true
         },
-        pieceName: {
-            type: String,
+        pieceInfo: {
+            type: Array,
             required: false
+        },
+        isSelected: {
+            type: Boolean,
+            required: false,
+            default: false
         }
     })
 
-    const tileColorStr = (props.isWhite ? "whiteTile" : "blackTile")
+    const emits = defineEmits(['selectPiece'])
+    const tileColor = (props.isWhite ? "whiteTile" : "blackTile")
+    const tileColorStr = computed(() => {return props.isSelected ? "selectedTile" : tileColor})
     const x = props.cordinates[0]
     const y = props.cordinates[1]
+    const imageStr = computed(() => {
+        let str = null
+        if(props.pieceInfo == null)
+            return str;
+        if(props.pieceInfo[0] == "w") {
+            switch (props.pieceInfo[1]) {
+                case "R":
+                    str = "/images/Chess_rlt60.png"
+                    break;
+                case "N":
+                    str = "/images/Chess_nlt60.png"
+                    break;
+                case "B":
+                    str = "/images/Chess_blt60.png"
+                    break;
+                case "Q":
+                    str = "/images/Chess_qlt60.png"
+                    break;
+                case "K":
+                    str = "/images/Chess_klt60.png"
+                    break;
+                case "p":
+                    str = "/images/Chess_plt60.png"
+                    break;
+            }
+        } else if(props.pieceInfo[0] == "b") {
+            switch (props.pieceInfo[1]) {
+                case "R":
+                    str = "/images/Chess_rdt60.png"
+                    break;
+                case "N":
+                    str = "/images/Chess_ndt60.png"
+                    break;
+                case "B":
+                    str = "/images/Chess_bdt60.png"
+                    break;
+                case "Q":
+                    str = "/images/Chess_qdt60.png"
+                    break;
+                case "K":
+                    str = "/images/Chess_kdt60.png"
+                    break;
+                case "p":
+                    str = "/images/Chess_pdt60.png"
+                    break;
+            }
+        }
+        return str
+    })
+
+    function selectPiece() {
+        emits('selectPiece', {
+            pieceInfo: props.pieceInfo,
+            index: props.index
+        })
+    }
 </script>
--- src/components/Chessboard/StandardBoard.vue
+++ src/components/Chessboard/StandardBoard.vue
@@ -1,22 +1,22 @@
 <template>
     <div class="boardGrid">
-        <BoardTile v-for="tile in tiles" :cordinates="tile.cordinates" :is-white="tile.isWhite" :piece-name="tile.pieceName"/>
+        <BoardTile v-for="tile in tiles" :index="tile.index" :cordinates="tile.cordinates" :is-white="tile.isWhite" :piece-info="tile.pieceInfo" :is-selected="tile.isSelected" @selectPiece="handleSelectPiece"/>
     </div>
 </template>
 
 <style>
     .boardGrid {
         display: grid;
-        grid-template-columns: repeat(8, 120px);
-        grid-template-rows: repeat(8, 120px);
+        grid-template-columns: repeat(8, 100px);
+        grid-template-rows: repeat(8, 100px);
         width: fit-content;
         height: fit-content;
     }
 </style>
 
 <script setup>
+import { ref } from 'vue';
 import BoardTile from './BoardTile.vue';
-
     const props = defineProps({
         hasWhite: {
             type: Boolean,
@@ -28,11 +28,14 @@ import BoardTile from './BoardTile.vue';
         }
     })
 
-    let tiles = []
+    let selectedTileIndex = null
+    let tiles = ref([])
     let tileColor = true
     let cordinatesX = Array[8]
     let cordinatesY = Array[8]
 
+    // Creating cordinates in an order based on player color
+    // This seemed easier than flipping the direction the tiles were created
     if(props.hasWhite) {
         cordinatesX = ['a','b','c','d','e','f','g','h']
         cordinatesY = ['8','7','6','5','4','3','2','1']
@@ -41,25 +44,38 @@ import BoardTile from './BoardTile.vue';
         cordinatesY = ['1','2','3','4','5','6','7','8']
     }
     
+    // Creating the array of tiles based on the given position
     for(let i = 0; i < 8; i++) {
         const y = cordinatesY[i]
         for(let j = 0; j < 8; j++) {
             const x = cordinatesX[j]
 
-            let pieceName = props.position.get(x+y)
-            if(!pieceName) {
-                pieceName = "none"
-            }
-            console.log(pieceName);
+            let pieceInfo = props.position.get(x+y)
 
             let tile = {
+                index: i*8+j,
                 cordinates: [x,y],
                 isWhite: tileColor,
-                pieceName: pieceName
+                pieceInfo: pieceInfo
             }
-            tiles.push(tile)
+            tiles.value.push(tile)
             if(j%8 != 7)
                 tileColor = !tileColor
+        }
+    }
+
+    function handleSelectPiece(data) {
+        if(selectedTileIndex != null){
+            if(selectedTileIndex != data.index){
+                tiles.value[data.index].pieceInfo = tiles.value[selectedTileIndex].pieceInfo
+                tiles.value[selectedTileIndex].pieceInfo = null
+            }
+            tiles.value[selectedTileIndex].isSelected = false
+            selectedTileIndex = null
+        }
+        else if(data.pieceInfo){
+            selectedTileIndex = data.index
+            tiles.value[data.index].isSelected = true
         }
     }
 
